29ff300cbd471a645a007f00ce5933180966a110
weijiew <836678589@qq.com>
2022-02-04 16:41:56 +0800

    finished lab2 ex2 IntegerAggregatorTest

--- src/java/simpledb/execution/IntegerAggregator.java
+++ src/java/simpledb/execution/IntegerAggregator.java
@@ -1,7 +1,17 @@
 package simpledb.execution;
 
+import simpledb.common.DbException;
 import simpledb.common.Type;
+import simpledb.storage.Field;
+import simpledb.storage.IntField;
 import simpledb.storage.Tuple;
+import simpledb.storage.TupleDesc;
+import simpledb.transaction.TransactionAbortedException;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 /**
  * Knows how to compute some aggregate over a set of IntFields.
@@ -9,6 +19,24 @@ import simpledb.storage.Tuple;
 public class IntegerAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
+
+    private int gbField;
+
+    private Type gbFieldType;
+
+    private int aField;
+
+    private Op what;
+
+    private int count;
+
+    private int sum;
+
+    // field sum
+    private HashMap<Field , Integer> fieldSum;
+
+    // field count
+    private HashMap<Field , Integer> fieldCount;
 
     /**
      * Aggregate constructor
@@ -27,6 +55,17 @@ public class IntegerAggregator implements Aggregator {
 
     public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+        this.gbField = gbfield;
+        this.gbFieldType = gbfieldtype;
+        this.aField = afield;
+        this.what = what;
+        if (gbfield != Aggregator.NO_GROUPING) {
+            fieldSum = new HashMap<>();
+            fieldCount = new HashMap<>();
+        }else {
+            count = 0;
+            sum = 0;
+        }
     }
 
     /**
@@ -37,7 +76,58 @@ public class IntegerAggregator implements Aggregator {
      *            the Tuple containing an aggregate field and a group-by field
      */
     public void mergeTupleIntoGroup(Tuple tup) {
-        // some code goes here
+        if (gbField != Aggregator.NO_GROUPING) {
+            Field field = tup.getField(gbField);
+            if (fieldSum.containsKey(field)) {
+                if (this.what == Op.AVG) {
+                    fieldSum.put(field , fieldSum.get(field) +
+                            ((IntField)tup.getField(aField)).getValue());
+                    fieldCount.put(field , fieldCount.get(field) + 1);
+                }else if (this.what == Op.COUNT) {
+                    fieldCount.put(field , fieldSum.get(field) + 1);
+                }else if (this.what == Op.SUM) {
+                    fieldSum.put(field , fieldSum.get(field) +
+                            ((IntField)tup.getField(aField)).getValue());
+                }else if (this.what == Op.MAX) {
+                    fieldSum.put(field , Math.max(fieldSum.get(field) ,
+                                    ((IntField)tup.getField(aField)).getValue()));
+                }else if (this.what == Op.MIN) {
+                    fieldSum.put(field , Math.min(fieldSum.get(field) ,
+                            ((IntField)tup.getField(aField)).getValue()));
+                }
+            } else {
+                if (this.what == Op.AVG) {
+                    fieldSum.put(field , ((IntField)tup.getField(aField)).getValue());
+                    fieldCount.put(field , 1);
+                }else if (this.what == Op.COUNT) {
+                    fieldCount.put(field ,  1);
+                }else if (this.what == Op.SUM) {
+                    fieldSum.put(field , ((IntField)tup.getField(aField)).getValue());
+                }else if (this.what == Op.MAX) {
+                    fieldSum.put(field , ((IntField)tup.getField(aField)).getValue());
+                }else if (this.what == Op.MIN) {
+                    fieldSum.put(field , ((IntField)tup.getField(aField)).getValue());
+                }
+            }
+        }else {
+            if (this.what == Op.AVG) {
+                count++;
+                sum += ((IntField)tup.getField(aField)).getValue();
+            }else if (this.what == Op.COUNT) {
+                count++;
+                sum++;
+            }else if (this.what == Op.SUM) {
+                sum += ((IntField)tup.getField(aField)).getValue();
+            }else if (this.what == Op.MAX) {
+                int t = ((IntField)tup.getField(aField)).getValue();
+                if (count == 0) sum = t;
+                else sum = Math.max(sum , t);
+            }else if (this.what == Op.MIN) {
+                int t = ((IntField)tup.getField(aField)).getValue();
+                if (count == 0) sum = t;
+                else sum = Math.min(sum , t);
+            }
+        }
     }
 
     /**
@@ -50,8 +140,109 @@ public class IntegerAggregator implements Aggregator {
      */
     public OpIterator iterator() {
         // some code goes here
-        throw new
-        UnsupportedOperationException("please implement me for lab2");
+        return new OpIterator() {
+
+            Iterator<Map.Entry<Field, Integer>> iterator;
+
+            TupleDesc td;
+
+            Tuple tuple;
+
+            private Boolean hasNext;
+
+            @Override
+            public void open() throws DbException, TransactionAbortedException {
+                if (gbField == Aggregator.NO_GROUPING) {
+                    if (td == null) {
+                        Type[] typeArr = new Type[1];
+                        typeArr[0] = Type.INT_TYPE;
+                        td = new TupleDesc(typeArr);
+                        tuple = new Tuple(td);
+                    }
+                    hasNext = true;
+                }else {
+                    iterator = fieldSum.entrySet().iterator();
+                    if (td == null) {
+                        Type[] typeArr = new Type[2];
+                        typeArr[0] = gbFieldType;
+                        typeArr[1] = Type.INT_TYPE;
+                        td = new TupleDesc(typeArr);
+                        tuple = new Tuple(td);
+                    }
+                }
+            }
+
+            @Override
+            public boolean hasNext() throws DbException, TransactionAbortedException {
+                if (gbField != Aggregator.NO_GROUPING) {
+                    if (iterator == null) throw new DbException("not yet open");
+                    if (iterator.hasNext()) return true;
+                    else return false;
+                }else {
+                    if (iterator == null) throw new DbException("not yet open");
+                    return hasNext;
+                }
+            }
+
+            @Override
+            public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
+                if (gbField != NO_GROUPING) {
+                    if (iterator == null) throw new NoSuchElementException();
+                    Map.Entry<Field, Integer> next = iterator.next();
+                    tuple.setField(0, next.getKey());
+                    if (what == Op.AVG) {
+                        Field field = new IntField(next.getValue() / fieldCount.get(next.getKey()));
+                        tuple.setField(1, field);
+                    } else {
+                        Field field = new IntField(next.getValue());
+                        // System.out.println(next.getValue());
+                        tuple.setField(1, field);
+                    }
+                } else {
+                    if (td == null) throw new NoSuchElementException();
+                    if (what == Op.AVG) {
+                        Field field = new IntField(sum / count);
+                        tuple.setField(0, field);
+                    } else {
+                        Field field = new IntField(sum);
+                        tuple.setField(0, field);
+                    }
+                    hasNext = false;
+                }
+                return tuple;
+            }
+
+            @Override
+            public void rewind() throws DbException, TransactionAbortedException {
+                open();
+            }
+
+            @Override
+            public TupleDesc getTupleDesc() {
+                if (gbField != NO_GROUPING) {
+                    if (td == null) {
+                        Type[] typeArr = new Type[2];
+                        typeArr[0] = gbFieldType;
+                        typeArr[1] = Type.INT_TYPE;
+                        return new TupleDesc(typeArr);
+                    }
+                } else {
+                    if (td == null) {
+                        Type[] typeArr = new Type[1];
+                        typeArr[0] = Type.INT_TYPE;
+                        return new TupleDesc(typeArr);
+                    }
+                }
+                return null;
+            }
+
+            @Override
+            public void close() {
+                iterator = null;
+                td = null;
+                tuple = null;
+            }
+        };
     }
 
 }
